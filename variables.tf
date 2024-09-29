206651b2c325c7f4f2ace0f0b671f434bfd9baa6
Junior Salvador <junioronliner@gmail.com>
2024-09-29 20:10:34 -0300

    feat,: container images and remove ecr creation

--- variables.tf
+++ variables.tf
@@ -2,6 +2,11 @@ variable "region" {}
 
 variable "service_name" {}
 
+variable "container_image" {
+  type = string
+  description = "Imagem com tag para deployment  da aplicaçao no ECS"
+}
+
 variable "cluster_name" {}
 
 variable "vpc_id" {}
